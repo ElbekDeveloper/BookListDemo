0c7096e45db0f346485cb0bf10b372b46c58545a
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-09-11 08:18:13 +0500

    Add database tables

---
+++ RazorUI/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorUI.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<BookModel> BookModels { get; set; }
+    }
+}
